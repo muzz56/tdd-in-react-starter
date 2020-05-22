b8abecb956355d4bcdd05078f7607b84a3fafbe0
muzz56 <muzammil0526@gmail.com>
2020-05-21 22:51:09 -0400

    Passes all the tests

---
+++ src/index.js
@@ -0,0 +1,8 @@
+ import React from "react";
+ import JokeGenerator from "./jokeGenerator";
+
+ const App = () => {
+   <div style={ styles }>
+     <JokeGenerator />
+   </div>
+ };
---
+++ src/joke.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default ({ text }) => (
+    <div data-testid="joke-text">
+        { text }
+    </div>
+);
+
+
